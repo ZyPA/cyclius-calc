be4041c21083426cc7868e9aca094dd28d35574b
ZyPA <rytterkasper972@gmail.com>
2023-04-02 15:56:20 +0200

    Added UTC to the slider label to make it more clear what timezone it was in.

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,7 @@ var output = document.getElementById("overrideTime");
 function handleTimer(value) {
   var hour = Math.floor(value) < 10 ? "0" + Math.floor(value) : Math.floor(value);
   var min = Math.floor((value - hour)*60) < 10 ? "0" + Math.floor((value - hour)*60) : Math.floor((value - hour)*60);
-  return `${hour}:${min}`
+  return `${hour}:${min} UTC`
 }
 
 output.innerHTML = handleTimer(slider.value);
