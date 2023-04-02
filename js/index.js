0eb0db878e66d02432f0ba9b09a3bc2390cda4d8
ZyPA <rytterkasper972@gmail.com>
2023-04-02 14:55:04 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,99 +1,102 @@
 const slots = {
   diamond: {
-    e: document.querySelector('.diamond .value'),
+    e: document.querySelector('.slot.diamond .slot-value'),
+    bestE: document.querySelector('.slot.diamond .slot-isBest'),
     cycle: 3,
   },
   ruby: {
-    e: document.querySelector('.ruby .value'),
+    e: document.querySelector('.slot.ruby .slot-value'),
+    bestE: document.querySelector('.slot.ruby .slot-isBest'),
     cycle: 12,
   },
   jade: {
-    e: document.querySelector('.jade .value'),
+    e: document.querySelector('.slot.jade .slot-value'),
+    bestE: document.querySelector('.slot.jade .slot-isBest'),
     cycle: 24,
   },
 };
 
-getAverage = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
+let activeSlot = 'none';
+document.querySelectorAll('.slot-isActive').forEach((item) => {
+  item.addEventListener('click', () => {
+    activeSlot = document.querySelector('.slot-isActive:checked').id;
+  });
+});
 
-function getMult(time, cycle, formatted = false) {
-  var floatingPoint = 2;
-  var _result = Math.sin(time * (Math.PI / cycle) * 2) * 15;
-  if (formatted) {
-    if (_result > 0) {
-      return `+${_result.toFixed(floatingPoint)}%`;
-    } else {
-      return `${_result.toFixed(floatingPoint)}%`;
-    }
+const avg = (arr) => arr.reduce((acc, curr) => acc + curr) / arr.length;
+
+function beautify(num) {
+  var _num = Math.abs(num).toFixed(2);
+  return num >= 0 ? `+${_num}%` : `-${_num}%`;
+}
+
+function getMult(time, slot) {
+  if (slots.hasOwnProperty(slot)) {
+    var cycle = slots[slot].cycle;
   } else {
-    return _result;
+    return 0;
   }
+  return Math.sin(time * (Math.PI / cycle) * 2) * 15;
 }
 
 function getBest(time) {
-  var diamondAvg = [],
+  var diaAvg = [],
     rubyAvg = [],
     jadeAvg = [];
+  const current = getMult(time, activeSlot),
+    dia = getMult(time, 'diamond'),
+    ruby = getMult(time, 'ruby'),
+    jade = getMult(time, 'jade');
 
   for (let i = 1; i <= 60; i++) {
-    diamondAvg.push(getMult(time + i / 60, slots.diamond.cycle));
-    rubyAvg.push(getMult(time + i / 60, slots.ruby.cycle));
-    jadeAvg.push(getMult(time + i / 60, slots.jade.cycle));
+    diaAvg.push(getMult(time + i / 60, 'diamond'));
+    rubyAvg.push(getMult(time + i / 60, 'ruby'));
+    jadeAvg.push(getMult(time + i / 60, 'jade'));
   }
 
-  diamondAvg = getAverage(diamondAvg);
-  rubyAvg = getAverage(rubyAvg);
-  jadeAvg = getAverage(jadeAvg);
+  diaAvg = avg(diaAvg);
+  rubyAvg = avg(rubyAvg);
+  jadeAvg = avg(jadeAvg);
 
-  if (diamondAvg > rubyAvg && diamondAvg > jadeAvg && diamondAvg > 0) {
-    return 'diamond';
-  } else if (rubyAvg > diamondAvg && rubyAvg > jadeAvg && rubyAvg > 0) {
-    return 'ruby';
-  } else if (jadeAvg > diamondAvg && jadeAvg > rubyAvg && jadeAvg > 0) {
+  if (jadeAvg >= rubyAvg && jadeAvg >= diaAvg && jade >= current && jade >= 0)
     return 'jade';
-  } else {
-    return 'none';
-  }
+  else if (
+    rubyAvg >= jadeAvg &&
+    rubyAvg >= diaAvg &&
+    ruby >= current &&
+    ruby >= 0
+  )
+    return 'ruby';
+  else if (diaAvg >= jadeAvg && diaAvg >= rubyAvg && dia >= current && dia >= 0)
+    return 'diamond';
+  else if (current >= 0) return activeSlot;
+  else return 'none';
 }
 
 function update() {
-  const now = new Date().getTime() / 3600000;
-  for (const [key, slot] of Object.entries(slots)) {
-    rawValue = getMult(now, slot.cycle);
-    nextValue = getMult(now + 1 / 3600000, slot.cycle);
-    value = getMult(now, slot.cycle, true);
-    if (
-      getBest(now) == key &&
-      !slot.e.parentElement.classList.contains('isBest')
-    ) {
-      slot.e.parentElement.classList.add('isBest');
-    } else if (
-      getBest(now) != key &&
-      slot.e.parentElement.classList.contains('isBest')
-    ) {
-      slot.e.parentElement.classList.remove('isBest');
+  var t = new Date().getTime() / 1000 / 60 / 60;
+  var currBest = getBest(t);
+  for ([key, item] of Object.entries(slots)) {
+    var mult = getMult(t, key);
+    var multNext = getMult(t + 1 / 1000, key);
+    item.e.setAttribute('data-increasing', mult < multNext ? 'true' : 'false');
+    if (currBest == key) {
+      document.querySelectorAll('[data-is-best="true"]').forEach((item) => {
+        if (!item.parentNode.classList.contains(key)) {
+          item.setAttribute('data-is-best', 'false');
+        }
+      });
+      item.bestE.setAttribute('data-is-best', 'true');
     }
-    if (slot.e.innerHTML != value) slot.e.innerHTML = value;
-    switch (rawValue < nextValue) {
-      case false:
-        slot.e.setAttribute('data-state', 'decreasing');
-        break;
-      case true:
-        slot.e.setAttribute('data-state', 'increasing');
-        break;
-      default:
-        slot.e.setAttribute('data-state', '');
-        break;
-    }
-    if (rawValue < 0) {
-      if (!slot.e.classList.contains('clr-red')) {
-        slot.e.classList.remove('clr-green');
-        slot.e.classList.add('clr-red');
-      }
-    } else if (rawValue > 0) {
-      if (!slot.e.classList.contains('clr-green')) {
-        slot.e.classList.remove('clr-red');
-        slot.e.classList.add('clr-green');
-      }
+    if (item.e.innerHTML != beautify(mult)) item.e.innerHTML = beautify(mult);
+    if (mult >= 0) {
+      if (item.e.classList.contains('clr-red'))
+        item.e.classList.remove('clr-red');
+      item.e.classList.add('clr-green');
+    } else if (mult < 0) {
+      if (item.e.classList.contains('clr-green'))
+        item.e.classList.remove('clr-green');
+      item.e.classList.add('clr-red');
     }
   }
   requestAnimationFrame(update);
